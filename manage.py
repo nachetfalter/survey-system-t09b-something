0218fde1f0d8607dfa147b5b00bd0da52076064f
zyy199807 <zyy199807@gmail.com>
2017-10-23 22:40:09 +1100

    third iteration merge

--- manage.py
+++ manage.py
@@ -8,9 +8,10 @@ ZHENYU YAO z5125769 2017-10
 
 
 import os
+import sys
 
 from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager, Shell
+from flask_script import Manager, Shell, Server
 from app import initialize_app, sqlalchemy as db
 
 
@@ -19,19 +20,32 @@ app = initialize_app(config)
 manager = Manager(app)
 migrate = Migrate(app, db)
 
-print('===== now using config {} ====='.format(config.upper()))
 
 def make_shell_context():
-    from app.model.models import User, Question, Result, Survey
+    ''' create shell context for app instance '''
+    from app.model.models import User, Question, Result, \
+                                 Survey, Choice, Survey_Question, \
+                                 Enrolment, Answer_Record, Course
     return dict(app=app, db=db, User=User, Question=Question,
-                Survey=Survey, Result=Result)
+                Survey=Survey, Result=Result, Choice=Choice,
+                Survey_Question=Survey_Question, Enrolment=Enrolment,
+                Answer_Record=Answer_Record, Course=Course)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("migrate_db", MigrateCommand)
 
+try:
+    if sys.argv[1] in ['runserver', 'shell']:
+        print('===== now using config {} ====='.format(config.upper()))
+except IndexError:
+    pass
 
 @manager.command
-def db_migrate():
-    return MigrateCommand
+def test():
+    ''' run unittests '''
+    import unittest
+    tests = unittest.TestLoader().discover('unittests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 
 if __name__ == "__main__":
